095e27c9d928e24df57324674dcd878cc49a1334
Tricodeit <bayu165kusuma@gmail.com>
2021-04-15 16:03:40 +0700

    Move Activity

---
+++ app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_avtivity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_avtivity -> {
+
+            }
+        }
+    }
+
+}
